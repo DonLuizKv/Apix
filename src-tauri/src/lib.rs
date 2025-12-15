947026300e94d685ced952df52f86ea5be967967
DonLuizKv <luizkv4@outlook.com>
2025-12-15 17:16:31 -0500

    updated dependences for rust and updated the request method in backend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,49 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Request {
+    method: String,
+    url: String,
+    headers: Vec<(String, String)>,
+    body: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Response {
+    status: u16,
+    headers: Vec<(String, String)>,
+    body: String,
+}
+
 #[tauri::command]
-fn request() {
-    println!("Request");
+async fn request(request: Request) -> Result<Response, String> {
+    // URL de tu backend real
+    let url = request.url;
+
+    // Creamos el cliente HTTP
+    let client = reqwest::Client::new();
+
+    // Hacemos la petición POST
+    let res = client
+        .post(url)
+        .json(&request.body) // reqwest serializa automáticamente a JSON
+        .send()
+        .await
+        .map_err(|e| e.to_string())?; // Convertimos error de red a String para Tauri
+
+    // Verificamos si el status es 200 OK
+    if res.status().is_success() {
+        // Parseamos la respuesta JSON a nuestra estructura Rust
+        let response_body = res
+            .json::<Response>()
+            .await
+            .map_err(|e| e.to_string())?;
+
+        Ok(response_body)
+    } else {
+        // Manejamos errores del servidor (ej. 400, 500)
+        Err(format!("Error del servidor: {}", res.status()))
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]

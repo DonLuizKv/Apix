2e6dd1dea80d4f5aed93eed9f5a7737c091fdb2d
DonLuizKv <luizkv4@outlook.com>
2025-12-19 21:14:31 -0500

    feat: Add Request component with form for sending API requests.

--- src/components/sections/Request.tsx
+++ src/components/sections/Request.tsx
@@ -4,12 +4,15 @@ export default function Request() {
 
     const { request } = useAPIContext();
 
-    const handleRequest = () => {
-        console.log("Hello");
-        
+    const handleRequest = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const method = e.currentTarget.method;
+        const url = e.currentTarget.url.value;
+
         request(
-            "GET",
-            "http://localhost:400",
+            method,
+            url,
             {
                 "Content-Type": "application/json"
             },
@@ -18,8 +21,17 @@ export default function Request() {
     };
 
     return (
-        <section className="w-[55%] max-sm:w-full h-full flex flex-col p-4 bg-amber-700">
-            <button type="button" onClick={handleRequest}>Send</button>
+        <section className="w-[55%] max-sm:w-full h-full flex flex-col p-4">
+            <form onSubmit={handleRequest}>
+                <select name="method">
+                    <option value="GET">GET</option>
+                    <option value="POST">POST</option>
+                    <option value="PUT">PUT</option>
+                    <option value="DELETE">DELETE</option>
+                </select>
+                <input type="text" name="url" className="border-input"/>
+                <button type="submit">Send</button>
+            </form>
         </section>
     );
 }
